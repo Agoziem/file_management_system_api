99f9bb621edc7322b410f6edebd2508682f88e73
GDD-impact <gddimpactsoftwares@gmail.com>
2025-10-02 10:08:26 +0100

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-# Wait for PostgreSQL to become available and accept connections
-echo "Waiting for Postgres..."
-export PGPASSWORD="$POSTGRES_PASSWORD" # PGPASSWORD is needed by pg_isready
-until pg_isready -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
-  echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-echo "Postgres is available. Continuing..."
-
 # Run Alembic migrations
 echo "Running Alembic migrations..."
 uv run alembic upgrade head
